78c6fcc679c6352bf61fe7bce3c4a710548ec10c
wpineda <wpineda@cultura.gob.sv>
2024-09-11 07:58:18 -0600

    [New]: button

--- src/components/DriveClone.vue
+++ src/components/DriveClone.vue
@@ -3,5 +3,23 @@ import DriveClone from './DriveClone.vue'
 </script>
 
 <template>
-  <div class="drive">HOLA</div>
+  <div class="drive">
+    <div class="drive-button">
+      <a href="#">Solicitar acceso</a>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+  .drive-button a{
+    width: 200px;
+    height: 200px;
+    background-color: #0b57cf;
+    color: white;
+    text-align: center;
+    font-size: 14px;
+    border-radius: 18px;
+    cursor: pointer;
+    padding: 10px 20px;
+  }
+</style>
